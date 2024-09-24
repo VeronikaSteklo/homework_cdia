65b758fad7e87275f6211df0e3a99558d88bb282
Veronika_Steklo <151031140+VeronikaSteklo@users.noreply.github.com>
2024-09-24 13:01:39 +0400

    Update practic5.3_var3.cs

--- practic5.3_var3.cs
+++ practic5.3_var3.cs
@@ -8,7 +8,7 @@ namespace pr53
             Console.Write("Введите натуральное число: ");
             int num = int.Parse(Console.ReadLine());
             int coint = Coint_num(num);
-            Console.WriteLine(coint);
+            Console.WriteLine($"Количество цифр в заданном числе: {coint}");
         }
 
         static int Coint_num(int num)

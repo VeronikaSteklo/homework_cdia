1febb03a8e6b6324b3acb8ecfe50cc8eff8aebb4
Veronika_Steklo <151031140+VeronikaSteklo@users.noreply.github.com>
2025-03-02 13:12:48 +0400

    Add files via upload

---
+++ Practic_20_var3/List.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task20
+{
+    public class List
+    {
+        private Node head;
+        private Node tail;
+
+        public List()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void AddBegin(int nodeInfo)
+        {
+            Node r = new Node(nodeInfo);
+            if (head == null)
+            {
+                head = r;
+                tail = r;
+            }
+            else
+            {
+                r.Next = head;
+                head = r;
+            }
+        }
+
+        public void AddEnd(int nodeInfo)
+        {
+            Node r = new Node(nodeInfo);
+            if (head == null)
+            {
+                head = r;
+                tail = r;
+            }
+            else
+            {
+                tail.Next = r;
+                tail = r;
+            }
+        }
+
+        public int TakeBegin()
+        {
+            if (head == null)
+            {
+                throw new Exception("Список пуст");
+            }
+            else
+            {
+                Node r = head;
+                head = head.Next;
+                if (head == null)
+                {
+                    tail = null;
+                }
+                return r.Inf;
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return head == null;
+            }
+        }
+
+        public Node Find(Node begin, object key)
+        {
+            Node r = begin;
+            while (r != null)
+            {
+                if (((IComparable)(r.Inf)).CompareTo(key) == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    r = r.Next;
+                }
+            }
+            return r;
+        }
+
+        public Node Find(object key)
+        {
+            return Find(head, key);
+        }
+
+        public void Insert(Node begin, int item)
+        {
+            Node r = begin;
+            if (r != null)
+            {
+                Node p = new Node(item);
+                p.Next = r.Next;
+                r.Next = p;
+            }
+        }
+
+        public void Show(string outputFile)
+        {
+            Node r = head;
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                while (r != null)
+                {
+                    writer.Write("{0} ", r.Inf);
+                    r = r.Next;
+                }
+                writer.WriteLine();
+            }
+        }
+    }
+}
---
+++ Practic_20_var3/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task20
+{
+    public class Node
+    {
+        public int Inf { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int inf)
+        {
+            Inf = inf;
+            Next = null;
+        }
+    }
+}

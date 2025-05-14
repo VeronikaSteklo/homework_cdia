732e2f6dda77ace1a8eae1b10f5fd24962564be6
Veronika_Steklo <151031140+VeronikaSteklo@users.noreply.github.com>
2025-05-14 23:00:49 +0400

    Graphs 2 option 3

---
+++ Graph2/Graph.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Graph
+{
+    private class Node
+    {
+        private int[,] array;
+        private bool[] visited;
+
+        public int this[int i, int j]
+        {
+            get => array[i, j];
+            set => array[i, j] = value;
+        }
+
+        public int Size => array.GetLength(0);
+
+        public void ResetVisited()
+        {
+            for (int i = 0; i < Size; i++)
+                visited[i] = true;
+        }
+
+        public Node(int[,] a)
+        {
+            array = a;
+            visited = new bool[a.GetLength(0)];
+        }
+
+        public void Dfs(int v)
+        {
+            Console.Write($"{v} ");
+            visited[v] = false;
+
+            for (int u = 0; u < Size; u++)
+            {
+                if (array[v, u] != 0 && visited[u])
+                    Dfs(u);
+            }
+        }
+
+        public void Bfs(int v)
+        {
+            Queue<int> queue = new();
+            queue.Enqueue(v);
+            visited[v] = false;
+
+            while (queue.Count > 0)
+            {
+                v = queue.Dequeue();
+                Console.Write($"{v} ");
+
+                for (int u = 0; u < Size; u++)
+                {
+                    if (array[v, u] != 0 && visited[u])
+                    {
+                        queue.Enqueue(u);
+                        visited[u] = false;
+                    }
+                }
+            }
+        }
+
+        public long[] Dijkstra(int start, out int[] prev)
+        {
+            visited[start] = false;
+
+            int[,] cost = new int[Size, Size];
+            for (int i = 0; i < Size; i++)
+                for (int j = 0; j < Size; j++)
+                    cost[i, j] = (i == j || array[i, j] == 0) ? int.MaxValue : array[i, j];
+
+            long[] dist = new long[Size];
+            prev = new int[Size];
+            bool[] used = new bool[Size];
+
+            for (int i = 0; i < Size; i++)
+            {
+                dist[i] = cost[start, i];
+                prev[i] = start;
+            }
+
+            dist[start] = 0;
+            used[start] = true;
+
+            for (int i = 0; i < Size - 1; i++)
+            {
+                long min = long.MaxValue;
+                int w = -1;
+
+                for (int j = 0; j < Size; j++)
+                {
+                    if (!used[j] && dist[j] < min)
+                    {
+                        min = dist[j];
+                        w = j;
+                    }
+                }
+
+                if (w == -1) break;
+                used[w] = true;
+
+                for (int j = 0; j < Size; j++)
+                {
+                    if (!used[j] && cost[w, j] != int.MaxValue && dist[j] > dist[w] + cost[w, j])
+                    {
+                        dist[j] = dist[w] + cost[w, j];
+                        prev[j] = w;
+                    }
+                }
+            }
+
+            return dist;
+        }
+
+        public void BuildPathDijkstra(int a, int b, int[] p, ref Stack<int> path)
+        {
+            path.Push(b);
+            if (a == p[b])
+                path.Push(a);
+            else
+                BuildPathDijkstra(a, p[b], p, ref path);
+        }
+
+        public long[,] Floyd(out int[,] p)
+        {
+            long[,] a = new long[Size, Size];
+            p = new int[Size, Size];
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    a[i, j] = (i == j) ? 0 : (array[i, j] != 0 ? array[i, j] : long.MaxValue);
+                    p[i, j] = -1;
+                }
+            }
+
+            for (int k = 0; k < Size; k++)
+            {
+                for (int i = 0; i < Size; i++)
+                {
+                    for (int j = 0; j < Size; j++)
+                    {
+                        if (a[i, k] < long.MaxValue && a[k, j] < long.MaxValue)
+                        {
+                            long dist = a[i, k] + a[k, j];
+                            if (a[i, j] > dist)
+                            {
+                                a[i, j] = dist;
+                                p[i, j] = k;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return a;
+        }
+
+        public void BuildPathFloyd(int a, int b, int[,] p, ref List<int> path)
+        {
+            int k = p[a, b];
+            if (k != -1)
+            {
+                BuildPathFloyd(a, k, p, ref path);
+                path.Add(k);
+                BuildPathFloyd(k, b, p, ref path);
+            }
+        }
+    }
+
+    private Node graph;
+
+    public Graph(string filename)
+    {
+        using StreamReader file = new(filename);
+        int n = int.Parse(file.ReadLine());
+        int[,] a = new int[n, n];
+        for (int i = 0; i < n; i++)
+        {
+            string[] parts = file.ReadLine().Split(' ');
+            for (int j = 0; j < n; j++)
+                a[i, j] = int.Parse(parts[j]);
+        }
+
+        graph = new Node(a);
+    }
+
+    public void Show()
+    {
+        for (int i = 0; i < graph.Size; i++)
+        {
+            for (int j = 0; j < graph.Size; j++)
+                Console.Write($"{graph[i, j],4}");
+            Console.WriteLine();
+        }
+    }
+
+    public void Dfs(int v)
+    {
+        graph.ResetVisited();
+        graph.Dfs(v);
+        Console.WriteLine();
+    }
+
+    public void Bfs(int v)
+    {
+        graph.ResetVisited();
+        graph.Bfs(v);
+        Console.WriteLine();
+    }
+
+    public void Dijkstra(int v)
+    {
+        graph.ResetVisited();
+        long[] dist = graph.Dijkstra(v, out int[] prev);
+        Console.WriteLine($"Кратчайшие пути до вершины {v}:");
+
+        for (int i = 0; i < graph.Size; i++)
+        {
+            if (i == v) continue;
+            if (dist[i] == int.MaxValue) continue;
+            else
+            {
+                Console.Write($"От вершины {i}: длина = {dist[i]}, путь: ");
+                Stack<int> path = new();
+                graph.BuildPathDijkstra(v, i, prev, ref path);
+                Console.WriteLine(string.Join(" <- ", path));
+            }
+        }
+    }
+
+    public void Floyd()
+    {
+        long[,] dist = graph.Floyd(out int[,] prev);
+        for (int i = 0; i < graph.Size; i++)
+        {
+            for (int j = 0; j < graph.Size; j++)
+            {
+                if (i == j) continue;
+                if (dist[i, j] == long.MaxValue)
+                {
+                    Console.WriteLine($"Пути из вершины {i} в {j} не существует.");
+                }
+                else
+                {
+                    Console.Write($"Путь из {i} в {j}: длина = {dist[i, j]}, путь: ");
+                    List<int> path = new() { i };
+                    graph.BuildPathFloyd(i, j, prev, ref path);
+                    path.Add(j);
+                    Console.WriteLine(string.Join(" -> ", path));
+                }
+            }
+        }
+    }
+    
+    // Удаление ребра между вершинами a и b
+    public void RemoveEdge(int a, int b)
+    {
+        if (a >= 0 && b >= 0 && a < graph.Size && b < graph.Size)
+        {
+            graph[a, b] = 0;
+            graph[b, a] = 0; // если граф неориентированный
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: неверные индексы вершин.");
+        }
+    }
+}
---
+++ Graph2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Graph2;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "C:\\Users\\veron\\RiderProjects\\Graph1\\Graph2\\input.txt";
+
+        Graph g = new Graph(filePath);
+       
+        Console.Write("Введите значение вершины a: ");
+        int a = int.Parse(Console.ReadLine());
+        g.Dijkstra(a);
+    }
+}
